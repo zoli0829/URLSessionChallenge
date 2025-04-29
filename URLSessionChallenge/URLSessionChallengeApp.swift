c0ef4486176576bbf7fee08bf7d87e96a12f36d7
zoli0829 <zoli0829@gmail.com>
2025-04-29 05:26:25 +0100

    Initial commit

---
+++ URLSessionChallenge/URLSessionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionChallengeApp.swift
+//  URLSessionChallenge
+//
+//  Created by Zoltan Vegh on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
